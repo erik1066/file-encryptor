0a3ecbb7b237252d6582e357e7a89c4d6f50ab96
Erik Knudsen <31932266+erik1066@users.noreply.github.com>
2019-02-11 20:37:27 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# File Encryptor / Decryptor
+
+A C# and .NET Core 2.2 command-line app for encrypting and decrypting files. Runs on macOS, Linux, and Windows. 
+
+This app has been neither vetted nor tested for security and is intended for research and development purposes only. Please do not use for production security work.
+
+## License
+The repository utilizes code licensed under the terms of the Apache Software License and therefore is licensed under ASL v2 or later.
+
+This source code in this repository is free: you can redistribute it and/or modify it under the terms of the Apache Software License version 2, or (at your option) any later version.
+
+This source code in this repository is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Apache Software License for more details.
+
+You should have received a copy of the Apache Software License along with this program. If not, see https://www.apache.org/licenses/LICENSE-2.0.html.
+
+The source code forked from other open source projects will inherit its license.
---
+++ src/Program.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.IO;
+using CommandLine;
+
+class Program
+{
+    public class Options
+    {
+        public bool Decrypt => Mode.Equals("d", StringComparison.OrdinalIgnoreCase);
+
+        public bool Encrypt => Mode.Equals("e", StringComparison.OrdinalIgnoreCase);
+
+        [Option("mode", Required = true, HelpText = "Sets mode, use 'e' for encrypt and 'd' for decrypt.")]
+        public string Mode { get; set; }
+
+        [Option("infile", Required = true, HelpText = "Input file.")]
+        public string Infile { get; set; }
+
+        [Option("outfile", Required = true, HelpText = "Output file.")]
+        public string Outfile { get; set; }
+
+        [Option("pwd", Required = true, HelpText = "Password.")]
+        public string Password { get; set; }
+
+        [Option("init", Required = true, HelpText = "Initialization vector.")]
+        public string InitVector { get; set; }
+
+        [Option("salt", Required = true, HelpText = "Salt value.")]
+        public string Salt { get; set; }
+
+        [Option("iter", Required = true, HelpText = "Number of iterations.")]
+        public int Iterations { get; set; }
+    }
+
+    static int RunOptionsAndReturnExitCode(Options options)
+    {
+        if (options.Decrypt && options.Encrypt)
+        {
+            Console.WriteLine("Cannot both encrypt and decrypt at the same time.");
+            return 1;
+        }
+        else if (!options.Decrypt && !options.Encrypt)
+        {
+            Console.WriteLine("Must select either encryption or decryption mode.");
+            return 1;
+        }
+        else if (options.Infile.Equals(options.Outfile, StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine("Infile and outfile must be different.");
+            return 1;
+        }
+
+        if (options.Encrypt)
+        {
+            Program.EncryptFile(options.Infile
+            , options.Outfile
+            , options.Password
+            , options.InitVector
+            , options.Salt
+            , options.Iterations);
+        }
+        else if (options.Decrypt)
+        {
+            Program.DecryptFile(options.Infile
+            , options.Outfile
+            , options.Password
+            , options.InitVector
+            , options.Salt
+            , options.Iterations);
+        }
+
+        return 0;
+    }
+
+    static int HandleParseError(IEnumerable<Error> errs)
+    {
+        foreach (var error in errs)
+        {
+            Console.WriteLine(error);
+        }
+        return 1;
+    }
+
+    static int Main(string[] args)
+    {
+        CommandLine.Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
+            .WithNotParsed<Options>((errs) => HandleParseError(errs));
+
+        return 0;
+    }
+
+    public static void EncryptFile(string inputFileName, string outputFileName, string password, string initVector, string saltValue, int iterations)
+    {
+        using (FileStream fsInput = new FileStream(inputFileName,
+            FileMode.Open,
+            FileAccess.Read))
+        {
+
+            using (FileStream fsEncrypted = new FileStream(outputFileName,
+                            FileMode.Create,
+                            FileAccess.Write))
+            {
+
+                byte[] salt = Encoding.ASCII.GetBytes(saltValue);
+                Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt, iterations);
+                RijndaelManaged SymmetricKey = new RijndaelManaged();
+                SymmetricKey.Padding = PaddingMode.PKCS7;
+                SymmetricKey.KeySize = 256;
+                SymmetricKey.Mode = CipherMode.CBC;
+                SymmetricKey.Key = key.GetBytes(SymmetricKey.KeySize / 8);
+
+                GCHandle gch = GCHandle.Alloc(SymmetricKey.Key, GCHandleType.Pinned);
+
+                SymmetricKey.IV = ASCIIEncoding.ASCII.GetBytes(initVector);
+                SymmetricKey.BlockSize = 128;
+
+                initVector = string.Empty;
+                saltValue = string.Empty;
+
+                ICryptoTransform aesEncrypt = SymmetricKey.CreateEncryptor(SymmetricKey.Key, SymmetricKey.IV);
+                using (CryptoStream cryptostream = new CryptoStream(fsEncrypted,
+                                    aesEncrypt,
+                                    CryptoStreamMode.Write))
+                {
+                    byte[] bytearrayinput = new byte[fsInput.Length - 1];
+                    fsInput.Read(bytearrayinput, 0, bytearrayinput.Length);
+                    cryptostream.Write(bytearrayinput, 0, bytearrayinput.Length);
+                }
+
+#if WIN64
+                ZeroMemory(gch.AddrOfPinnedObject(), 32);
+#endif
+                gch.Free();
+
+                SymmetricKey.Clear();
+            }
+        }
+    }
+
+    public static void DecryptFile(string inputFileName, string outputFileName, string password, string initVector, string saltValue, int iterations)
+    {
+        byte[] salt = Encoding.ASCII.GetBytes(saltValue);
+        Rfc2898DeriveBytes key = new Rfc2898DeriveBytes(password, salt, iterations);
+        RijndaelManaged SymmetricKey = new RijndaelManaged();
+        SymmetricKey.Padding = PaddingMode.PKCS7;
+        SymmetricKey.KeySize = 256;
+        SymmetricKey.Mode = CipherMode.CBC;
+        SymmetricKey.Key = key.GetBytes(SymmetricKey.KeySize / 8);
+
+        GCHandle gch = GCHandle.Alloc(SymmetricKey.Key, GCHandleType.Pinned);
+
+        SymmetricKey.IV = ASCIIEncoding.ASCII.GetBytes(initVector);
+        SymmetricKey.BlockSize = 128;
+
+        initVector = string.Empty;
+        saltValue = string.Empty;
+
+        FileStream fsread = new FileStream(inputFileName,
+                                FileMode.Open,
+                                FileAccess.Read);
+
+        ICryptoTransform aesDecrypt = SymmetricKey.CreateDecryptor(SymmetricKey.Key, SymmetricKey.IV);
+
+        CryptoStream cryptostreamDecr = new CryptoStream(fsread,
+                                                        aesDecrypt,
+                                                        CryptoStreamMode.Read);
+        if (File.Exists(outputFileName))
+        {
+            File.Delete(outputFileName);
+        }
+
+        FileStream fsOut = new FileStream(outputFileName, FileMode.Create);
+
+        try
+        {
+            int data;
+            while ((data = cryptostreamDecr.ReadByte()) != -1)
+                fsOut.WriteByte((byte)data);
+
+            cryptostreamDecr.Flush();
+            cryptostreamDecr.Close();
+            cryptostreamDecr.Dispose();
+        }
+        catch (CryptographicException ex)
+        {
+            throw new CryptographicException("There was a problem decrypting the file. Check the password and try again.", ex);
+        }
+        finally
+        {
+            if (fsOut != null)
+            {
+                fsOut.Close();
+            }
+
+            fsread.Close();
+
+#if WIN64
+            ZeroMemory(gch.AddrOfPinnedObject(), 32);
+#endif
+            gch.Free();
+
+            SymmetricKey.Clear();
+        }
+    }
+
+    [System.Runtime.InteropServices.DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+    public static extern bool ZeroMemory(IntPtr Destination, int Length);    
+}
+
